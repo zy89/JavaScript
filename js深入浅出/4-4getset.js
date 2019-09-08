02cd7f38ff2d800d9a6013c9cd1061766eea9f66
zy89 <yzhiyuan0321@gmail.com>
2019-09-08 13:22:06 +1000

    4-4

--- js深入浅出/4-4getset.js
+++ js深入浅出/4-4getset.js
@@ -1,3 +1,4 @@
+
 //基本get/set
 var man ={
     name: 'yzy',
@@ -15,6 +16,7 @@ function foo(){
     console.log(man.age);//还是24
 }
 
+//赋值age的方法
 var man1={
     weibo:'@yzy',
     $age:null,
@@ -27,6 +29,7 @@ var man1={
     },
     set age(val){
         val = +val;//字符串转换为数字
+        //isNaN方法，检查一个变量是否能转换为数字，能转换返回false，不能返回true
         if(!isNaN(val) && val>0 && val<150){
             this.$age = +val;
         }else {
@@ -40,4 +43,22 @@ function foo1(){
     console.log(man1.age);
     man1.age ='abc';
 }
-foo1();
+//foo1();
+
+//get/set与原型链的关系
+
+function foo2(){
+    //在foo2里定义一个z，用get方法返回一个1
+    Object.defineProperty(foo2.prototype, 'z',{get:function(){return 1;}});
+}
+var obj2 =new foo2();
+console.log(obj2.z);
+obj2.z=10;
+console.log("z is still:"+obj2.z); //因为原型里有get函数，所以无法给new obj2里创建新的z
+//实现方法：
+Object.defineProperty(obj2,'z',{value:100,configurable:true});//定义z为100
+console.log("now obj2.z is: "+obj2.z);
+delete obj2.z;//删除obj2里的z
+console.log("delete obj2.z then z back to: "+obj2.z);
+
+
