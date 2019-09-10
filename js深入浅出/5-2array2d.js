21e9eb4264390e4f0af872280d191fbb077736b4
zy89 <yzhiyuan0321@gmail.com>
2019-09-10 14:34:47 +1000

    5-2

--- js深入浅出/5-2array2d.js
+++ js深入浅出/5-2array2d.js
@@ -1,3 +1,4 @@
+//1.二维数组
 var arr=[[0,1],[2,3],[4,5]];
 var row;
 for(var i=0;i<arr.length;i++){
@@ -7,3 +8,9 @@ for(var i=0;i<arr.length;i++){
         console.log(row[j]);//打印出小数组里的元素
     }
 }
+
+//2.稀疏数组：并不含有从0开始的连续索引
+var arr1 =[];
+arr1.length =100;//长度100
+arr1[99]=123;//最后一个元素是123
+console.log(arr[98]);//123以前没有任何元素
