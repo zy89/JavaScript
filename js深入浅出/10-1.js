29c9a6ac58d552f6ff56d3e5006a70b2e97bf05a
zy89 <yzhiyuan0321@gmail.com>
2019-09-17 15:03:41 +1000

    10-1

--- js深入浅出/10-1.js
+++ js深入浅出/10-1.js
@@ -20,3 +20,25 @@ new RegExp("yzy").test("hi,yzy");//大小写敏感
 // \s 空格符, tab, 换页符，换行符
 // \S 非\s
 // \t \r \n \v \f tab，回车，换行，垂直制表符，换页符
+
+//2.2 范围符号
+// [...]  表示字符范围；[a-z]表示小写字母a-z
+// [^...] 表示字符范围以外
+// ^ 表示匹配行首；^Hi表示Hi必须在最开头
+// $ 行尾
+// \b 零宽单词边界；\bno表示no前要有单词，直接开头no不会匹配
+// \B
+
+//2.3特殊符转义 用\
+var c=/\^abc/.test('^abc');//直接把^当作字符去判断
+console.log(c);//true
+
+//2.4分组
+// (x) 分组，并记录匹配到的字符串
+// \n 表示使用分组符(x)匹配到的字符串
+var d=/(abc)\1/.test('abcabc');// \1等价于abc，所以总共2个abc
+console.log(d);// true
+
+//(?:x) 表示仅分组
+var e=/(?:abc)(def)\1/.test('abcdefdef');//这样abc就不会被算入\1
+console.log(e);
